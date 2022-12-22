b130d499cb390a3368863f0d8987038e7111c70b
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2022-12-22 17:48:16 -0500

    change enviroment

--- core/settings.py
+++ core/settings.py
@@ -146,11 +146,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if not DEBUG:
   EMAIL_BACKEND   = 'django.core.mail.backends.smtp.EmailBackend'
-  EMAIL_HOST      = env('EMAIL_HOST')
-  EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-  EMAIL_HOST_PASSWORD  = env('EMAIL_HOST_PASSWORD')
-  EMAIL_PORT      = env('EMAIL_PORT')
-  EMAIL_USE_TLS   = env('EMAIL_USE_TLS')
+  EMAIL_HOST      = os.environ.get('EMAIL_HOST')
+  EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+  EMAIL_HOST_PASSWORD  = os.environ.get('EMAIL_HOST_PASSWORD')
+  EMAIL_PORT      = os.environ.get('EMAIL_PORT')
+  EMAIL_USE_TLS   = os.environ.get('EMAIL_USE_TLS')
   
   SESSION_COOKIE_SECURE = True
   SECURE_BROWSER_XSS_FILTER = True
